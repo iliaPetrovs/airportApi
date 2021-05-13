421d3da7ee77a0fc8aa3e32ea3de51b6dd281db6
Ilia Petrovs <ilja.petrovs@transferwise.com>
2021-05-13 12:26:49 +0100

    added hash

--- src/main/java/org/whitehat/airports/WebController.java
+++ src/main/java/org/whitehat/airports/WebController.java
@@ -1,13 +1,18 @@
 package org.whitehat.airports;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +24,28 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/airports")
 public class WebController {
     public List<String> airports = new ArrayList<>();
+    public Map<String, String> users = Map.of("test user", "password");
 
     public WebController() {
         airports.add("LHR");
         airports.add("NRT");
         airports.add("LAX");
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode("cabbage");
+
+        System.out.println(hashedPassword);
+
+        boolean isMatch = passwordEncoder.matches(hashedPassword, "$2a$10$MFDKo5y8czJsu4Gapjnk6uwCwKt1yblvtyu2rjMAE2QKh/H/UVAu6");
+        boolean isMatchTwo = passwordEncoder.matches("won't match", "$2a$10$MFDKo5y8czJsu4Gapjnk6uwCwKt1yblvtyu2rjMAE2QKh/H/UVAu6");
+        System.out.println(String.format("This should be true: %s. This should be false: %s", isMatch, isMatchTwo));
+
+    }
+
+    @PostMapping("/test")
+    public String makeUser(@RequestBody String user) throws IOException {
+        System.out.println(user);
+        return user;
     }
 
 
